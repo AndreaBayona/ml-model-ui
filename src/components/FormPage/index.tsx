f97e5c802873b68d99fafa9cf768bf14dba202fd
AndreaBayona <ma.bayona@uniandes.edu.co>
2022-02-23 16:25:54 -0500

    add text fields

--- src/components/FormPage/index.tsx
+++ src/components/FormPage/index.tsx
@@ -8,23 +8,12 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import SettingsIcon from '@mui/icons-material/Settings';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import InputAdornment from '@mui/material/InputAdornment';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const theme = createTheme();
 
@@ -52,59 +41,127 @@ export const FormPage: React.FC = () => {
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
+            <SettingsIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            Set up model variables
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
-                  autoComplete="given-name"
-                  name="firstName"
+                  name="latitude"
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
+                  id="latitude"
+                  label="Latitude"
+                  type="number"
+                  autoComplete="latitude"
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
+                  id="longitude"
+                  label="Longitude"
+                  name="longitude"
+                  type="number"
+                  autoComplete="longitude"
                 />
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
+                  id="region"
+                  label="Region"
+                  name="region"
+                  type="number"
+                  autoComplete="region"
                 />
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
+                  name="building_type"
+                  label="Building type"
+                  id="building_type"
+                  type="number"
+                  autoComplete="building_type"
                 />
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="object_type"
+                  label="Object type"
+                  id="object_type"
+                  type="number"
+                  autoComplete="object_type"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="level"
+                  label="Level"
+                  id="level"
+                  type="number"
+                  autoComplete="level"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="levels"
+                  label="Levels"
+                  id="levels"
+                  type="number"
+                  autoComplete="levels"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="rooms"
+                  label="Rooms"
+                  id="rooms"
+                  type="number"
+                  autoComplete="rooms"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="area"
+                  label="Area"
+                  id="area"
+                  type="number"
+                  autoComplete="area"
+                  InputProps={{
+                    endAdornment: <InputAdornment position="end">m^2</InputAdornment>,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="kitchen_area"
+                  label="Kitchen area"
+                  id="kitchen_area"
+                  type="number"
+                  autoComplete="kitchen_area"
+                  InputProps={{
+                    endAdornment: <InputAdornment position="end">m^2</InputAdornment>,
+                  }}
                 />
               </Grid>
             </Grid>
@@ -114,18 +171,10 @@ export const FormPage: React.FC = () => {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+              Predict
             </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
   );
