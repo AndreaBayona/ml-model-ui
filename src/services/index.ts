182b22c41507ab3eda0f93718418f6fbfc5670f7
AndreaBayona <ma.bayona@uniandes.edu.co>
2022-02-23 17:35:10 -0500

    add services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,17 @@
-export const API_IP = ""
+const SERVER = "localhost";
+const PORT = "3001";
+export const SERVER_URL = `http://${SERVER}:${PORT}`;
 
+type Method = "GET" | "POST" | "PUT";
+
+export const request = async (path: string, method: Method, body?: string) => {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Access-Control-Allow-Origin", "*");
+  headers.append("Access-Control-Allow-Credentials", "true");
+  headers.append("Access-Control-Allow-Headers", "*");
+
+  const response = await fetch(path, {method, mode: "cors", body, headers});
+  if (response.status === 200) return response.json();
+  else throw new Error("error" + response.status);
+}
